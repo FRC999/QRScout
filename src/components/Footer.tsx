9bd8a95d9f8dc3ad5375199082d9f7b0d6f2fd5b
CoachPrash <69826040+CoachPrash@users.noreply.github.com>
2024-03-22 13:07:38 -0400

    Added MechaRAMS site link to footer.

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,7 @@ export function Footer() {
   return (
     <footer>
       <div className="mt-8 flex h-24 flex-col items-center justify-center p-2">
-        <img src="src/assets/999BANNER.png"/>
+        <h3><a href="https://cheshirerobotics.org/">MechaRAMS - FRC Team 999</a></h3>
       </div>
     </footer>
   );
